25ca86af1d75c6e3aaaee17cc0452c62454883be
Max Burri <max.burri@gmail.com>
2015-08-22 00:08:09 +0200

    added posibility to create new pizzas

--- client/frontend-pizza.js
+++ client/frontend-pizza.js
@@ -3,3 +3,15 @@ Template.body.helpers({
         return Pizzas.find();
     }
 });
+
+Template.body.events({
+    'submit .add-pizza': function(event, template) {
+        event.preventDefault();
+        var text = event.target.flavor.value;
+        Pizzas.insert({
+            flavor: text,
+            votes: 0
+        });
+        event.target.flavor.value = '';
+    }
+});
